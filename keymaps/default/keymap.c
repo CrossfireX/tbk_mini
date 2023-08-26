4b91bced3224b1afca516e09c6e8c031a2bf958c
CrossfireX <borut.pezdicek@gmail.com>
2023-08-26 19:48:52 +0200

    aug 2023 fix

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -20,24 +20,12 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_split_3x6_3(
-<<<<<<< HEAD
         
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL,
         LALT_T(KC_ESC), KC_LGUI, LCTL_T(KC_TAB), KC_ENT, LT(1,KC_SPC), LT(2,KC_BSPC)
-=======
-        //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL,
-        //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-         LALT_T(KC_ESC), KC_LGUI, LCTL_T(KC_TAB), KC_ENT, LT(1,KC_SPC), LT(2,KC_BSPC)
-        //`--------------------------'  `--------------------------'
 
->>>>>>> e8123fad959abd8413e0dcead4c5b1a03db29903
         ),
 
     [1] = LAYOUT_split_3x6_3(
@@ -45,11 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5,                          KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
         KC_NO, KC_NO, KC_HOME, KC_PGUP, KC_PGDN, KC_END,             KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_DEL, KC_NO, KC_NO, KC_NO,                  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-<<<<<<< HEAD
         KC_NO, KC_NO, KC_NO,          KC_NO, KC_TRNS, MO(3)
-=======
-                      KC_NO, KC_NO, KC_NO,          KC_NO, KC_TRNS, MO(3)
->>>>>>> e8123fad959abd8413e0dcead4c5b1a03db29903
 
         ),
 
@@ -58,26 +42,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_EXLM, KC_AT, KC_HASH,  KC_DLR, KC_PERC,            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NO,
         KC_NO, KC_GRV, KC_QUOT, KC_NO, KC_NO, KC_NO,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_NO,
         KC_LSFT, KC_TILD, KC_DQUO, KC_NO,KC_NO, KC_NO,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_NO,
-<<<<<<< HEAD
         KC_NO, KC_NO, KC_NO,         KC_NO, MO(3), KC_NO
-=======
-                             KC_NO, KC_NO, KC_NO,         KC_NO, MO(3), KC_NO
->>>>>>> e8123fad959abd8413e0dcead4c5b1a03db29903
 
         ),
 
     [3] = LAYOUT_split_3x6_3(
         
-<<<<<<< HEAD
         QK_BOOT, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                          KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO,
         KC_NO, KC_F11, KC_F12, KC_MPRV, KC_MPLY, KC_MNXT,                    KC_MUTE, KC_VOLD, KC_VOLU, KC_BRID, KC_BRIU, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO
-=======
-        KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                            KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO,
-        KC_NO, KC_F11, KC_F12, KC_MPRV, KC_MPLY, KC_MNXT,                    KC_MUTE, KC_VOLD, KC_VOLU, KC_BRID, KC_BRIU, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                            KC_NO, KC_NO, QK_BOOT,           KC_NO, KC_NO, KC_NO
->>>>>>> e8123fad959abd8413e0dcead4c5b1a03db29903
         )
 };
